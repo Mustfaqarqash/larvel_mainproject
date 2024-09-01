e30d245c75a7f14a4566ca661b702fed046420f5
AbdallahAwaysheh <awaysheh6930@gmail.com>
2024-09-01 17:03:10 +0300

    Abdallah Upload Files

--- routes/web.php
+++ routes/web.php
@@ -9,29 +9,61 @@ use App\Http\Controllers\TestimonialController;
 use App\Http\Controllers\TripController;
 use App\Http\Controllers\TripImagesController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\EmailController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 
 Route::get('/', function () {
 return redirect()->route('home');
 });
-Route::get('/dash', function () {
-    return view('dashboard');
-})->middleware(['isAdmin'])->name('dashboard');
-Auth::routes();
+//------------------------------ contact routes------------------------------------
+Route::get('/contact',[EmailController::class,'contactForm']);
+Route::post('/contactMail',[EmailController::class,'contact'])->name('contact')->middleware('verified');
+//------------------------------ contact routes------------------------------------
 
-Route::get('/home',function () {
-    return view('landing_page');
-})->name('home');
+Route::get('/dash',[DashboardController::class, 'index'] )->middleware(['isAdmin'])->name('dashboard');
+
+Auth::routes( [
+    'verify' => true
+]);
+
+Route::get('/home',[HomeController::class,'index'])->name('home');
+
+//trips start --------------------------
+Route::resource('trips', TripController::class)->middleware(['auth' , 'isAdmin']);
+//trips end ----------------------------
+
+//category start ----------------------------
+Route::resource('categories', CategoryController::class)->middleware(['auth' , 'isAdmin']);
+Route::get('/categories/view/{id}', [CategoryController::class, 'view'])->name('categoties');
+//category end ------------------------------
+
+//dashboard start----------------------------
+Route::get('/dash',[DashboardController::class, 'index'] )->middleware(['isAdmin'])->name('dashboard');
+//dashboard end----------------------------
+
+//guide start----------------------------
+Route::resource('/guides', GuideController::class)->middleware(['auth', 'isAdmin']);
+//guide end----------------------------
+
+//testimonials start----------------------------
+Route::resource('testimonials', TestimonialController::class)->middleware(['auth' ]);
+//testimonials end----------------------------
+
+//testimonials start----------------------------
+Route::resource('/users', UserController::class)->middleware(['auth', 'isAdmin']);
+//testimonials end----------------------------
+
+
+Route::get('/tripimages/create/{id}', [TripImagesController::class, 'create'])->name('tripimages.create');
+Route::post('/tripimages/store/{id}', [TripImagesController::class, 'store'])->name('tripimages.store');
+
+Route::get('/tripguide/create/{id}', [GuideTripController::class, 'create'])->name('tripguide.create');
+Route::post('/tripguide/store/{id}', [GuideTripController::class, 'store'])->name('tripguide.store');
+
+
+//tripimages start----------------------------
+//Route::resource('/tripimages', TripImagesController::class)->middleware(['auth', 'isAdmin']);
+//tripimages end----------------------------
